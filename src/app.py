122dbf15cd19defd2f08a006d8ba0f37f59e23ef
Grizzly-val <lanz.10donatos@gmail.com>
2026-01-27 17:39:18 +0800

    http exceptions + logging tweaks

--- src/app.py
+++ src/app.py
@@ -17,7 +17,7 @@ import logging
 from redis.asyncio import Redis
 
 
-from src.dependencies import get_client, get_redis
+from src.dependencies import ServiceProvider
 
 from urllib.parse import urlencode
 
@@ -34,7 +34,7 @@ async def lifespan(app: FastAPI):
     app.state.client = httpx.AsyncClient(timeout=httpx.Timeout(10.0))
     app.state.redis = Redis(host="localhost", port=6379, decode_responses=True)
     app_logger.info("HTTP client started")
-    app_logger.info("Redis connection started")
+    app_logger.info("Redis connection started\n")
 
     yield
 
@@ -52,32 +52,49 @@ JIKAN_URL = "https://api.jikan.moe/v4"
 
 @app.exception_handler(RequestValidationError)
 async def request_validation(request: Request, exc: RequestValidationError):
-    app_logger.warning(f"User sent bad data: {exc.errors()}")
+    app_logger.warning(f"User sent bad data: {exc.errors()}\n")
 
     return JSONResponse(
         content = {"message": "You've entered invalid filter/s", "details": exc.errors()},
         status_code = status.HTTP_422_UNPROCESSABLE_CONTENT
     )
 
-# @app.exception_handler(HTTPException) Not now
+
+
+@app.exception_handler(HTTPException)
+async def http_exception_handler(request: Request, exc: HTTPException):
+    # Log the actual detail (which is what you passed to 'detail=...')
+    app_logger.warning(f"HTTP Error {exc.status_code}: {exc.detail}\n")
+
+    return JSONResponse(
+        status_code=exc.status_code, # Use the status code from the exception!
+        content={
+            "status_code": exc.status_code,
+            "detail": exc.detail,
+        }
+    )
+
+
+
 
 async def fetch_jikan(request_url: str, client: httpx.AsyncClient) -> httpx.Response:
     try:
         app_logger.info(f"Fetching Jikan | URL: {request_url}")
         response = await client.get(request_url)
+        json_response = response.json()
         response.raise_for_status()
 
-    except httpx.HTTPStatusError as httpError:
-        app_logger.warning(f"Fetch failed!  | HTTPStatus: {response.status_code}")
-    else:
+        if isinstance(json_response, dict) and "status" in json_response and json_response.get("status", 200) >= 400:
+            app_logger.warning(f"Fetch failed!  | HTTPStatus: {json_response["status"]}")
+            raise HTTPException(status_code=json_response.get("status", 400))
+        
         app_logger.info(f"Fetch successful! | HTTPStatus: {response.status_code}")
-    finally:
+
         return response
-
-async def check_redis(request_url: str, params: dict, redis: Redis):
-    return await redis.get()
-    ...
-
+    except httpx.HTTPStatusError as e:
+            app_logger.error(f"Upstream HTTP Error: {e.response.status_code}")
+            raise HTTPException(status_code=e.response.status_code, detail="Jikan Server Error",)
+    
 
 """
 ========================  TODO : CACHING ========================
@@ -114,43 +131,49 @@ Datas to cache:
 
 
 
-async def handle_request(client: httpx.AsyncClient, redis: Redis, lf: LayerOneParameters) -> tuple[dict, httpx.Response]:
-    params = lf.model_dump(exclude_none=True, exclude={"subject"}, mode="json")
+async def layer_one_handler(services: ServiceProvider, l1_params: LayerOneParameters) -> dict:
+    client = services.client
+    redis = services.redis
+
+    params = l1_params.model_dump(exclude_none=True, exclude={"subject"}, mode="json")
     query_string = urlencode(params)
-    request_url = f"{JIKAN_URL}/{lf.subject.value}?{query_string}" 
+    request_url = f"{JIKAN_URL}/{l1_params.subject.value}?{query_string}" 
 
 
     l1_cache = await redis.get(request_url)
     
-    cache_ttl = 30
+    ttl = 30
 
     if l1_cache is not None:
         app_logger.info(f"Cache hit for {request_url}")
+        app_logger.info(f"Fetching from cache\n")
         # Extend TTL: 'expire' resets the timer without changing the value
-        await redis.expire(request_url, cache_ttl)
+        await redis.expire(request_url, ttl)
         # Convert bytes from Redis back to a Python Dict
         return json.loads(l1_cache)
     
     try:
         app_logger.info(f"Cache miss. Fetching from Jikan...")
         jikan_response = await fetch_jikan(request_url, client)
-        data = jikan_response.json()
-
         jikan_response.raise_for_status()
-    except httpx.HTTPStatusError as httpError:
+
+        data = jikan_response.json()
+    except HTTPException as httpError:
         app_logger.warning(f"Caching cancelled: HTTP Status Error Occured")
-        return data
+        raise
     except Exception as e:
         app_logger.warning(f"Caching cancelled: Unknown Error Occured: {e}")
+        raise HTTPException(status_code=500, detail={"Unknown Error": "unknown"})
+    else:
+        app_logger.info(f"Caching query results\n")
+        json_string = json.dumps(data)
+        await redis.set(name=request_url, value=json_string, ex=ttl)
+
         return data
 
-    app_logger.info(f"Caching query results")
-    json_string = json.dumps(data)
-    await redis.set(name=request_url, value=json_string, ex=cache_ttl)
 
-    return data
-
-
+async def layer_two_handler(services: ServiceProvider) -> dict:
+    ... 
 
     
     
@@ -161,6 +184,8 @@ async def handle_request(client: httpx.AsyncClient, redis: Redis, lf: LayerOnePa
 
 
 @app.post("/get_recommendation", status_code=200)
-async def get_recommendation(lf: LayerOneParameters, client: httpx.AsyncClient = Depends(get_client), redis: Redis = Depends(get_redis)) -> dict:
-    response = await handle_request(client, redis, lf)
-    return response
+async def get_recommendation(l1_params: LayerOneParameters, services: ServiceProvider = Depends()) -> dict:
+    l1_response = await layer_one_handler(services, l1_params)
+    return l1_response
+
+    # l2_response = await layer_two_handler()
